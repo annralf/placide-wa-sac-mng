2e643f6dc5fc9a0c7f806791f2627fc974befa18
Alexis Güere <freealex2.0@gmail.com>
2021-06-02 23:01:28 -0400

    [fix] djongo

--- wa_sac_mng/settings/django.py
+++ wa_sac_mng/settings/django.py
@@ -100,7 +100,7 @@ DATABASES = {
 #            'host': 'mongodb+srv://admin:rVWMftMT4DjlIEAh@cluster0.g3uvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 #            'username': 'admin',
 #            'password': 'rVWMftMT4DjlIEAh',
-        }
+#        }
     }
 }
 
