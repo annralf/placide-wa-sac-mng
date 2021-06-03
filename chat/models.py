2f50f55fb9add892768f19960759744c97f74597
aguere <anaguere@gmail.com>
2021-06-02 21:22:33 -0500

    feat: merge models

--- chat/models.py
+++ chat/models.py
@@ -5,6 +5,8 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+from jsonfield import JSONField
+
 from django.db import models
 
 
@@ -179,7 +181,6 @@ class UsersStatus(models.Model):
     class Meta:
         managed = False
         db_table = 'users_status'
-from djongo import models
 
 class Agent(models.Model):
     agent_name = models.CharField(max_length = 20)
