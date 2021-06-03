6ef5177d593c37dd7d69280730ba98130d43371c
Alexis Güere <freealex2.0@gmail.com>
2021-06-02 21:38:37 -0400

    [feat] Added heroku configuration

--- chat/models.py
+++ chat/models.py
@@ -1,18 +1,18 @@
-from djongo import models
+#from djongo import models
 
-class Agent(models.Model):
-    agent_name = models.CharField(max_length = 20)
-    instances = models.JSONField(default = [])
+#class Agent(models.Model):
+#    agent_name = models.CharField(max_length = 20)
+#    instances = models.JSONField(default = [])
 
-class Message(models.Model):
-    raw_data = models.JSONField()
-    ws_id = models.CharField(max_length = 50)
-    assigned = models.BooleanField(default = False)
+#class Message(models.Model):
+#    raw_data = models.JSONField()
+#    ws_id = models.CharField(max_length = 50)
+#    assigned = models.BooleanField(default = False)
 
-    class Meta:
-        abstract = True
+#    class Meta:
+#        abstract = True
 
-class Queue(models.Model):
-    message = models.EmbeddedField(
-        model_container = Message
-    )
+#class Queue(models.Model):
+#    message = models.EmbeddedField(
+#        model_container = Message
+#    )
